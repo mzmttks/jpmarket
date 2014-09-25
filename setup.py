242f439ea71a78711ee9d49728d6587fbb989f41
mzmttks <ta.mizumoto@gmail.com>
2014-09-25 23:57:26 +0900

    add the initial files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name = 'jpmarket',
+      version = '0.1',
+      description = 'pandas extension of DataReader for Japanese market data',
+      author = 'Takeshi Mizumoto',
+      url = 'https://github.com/mzmttks/jpmarket',
+      packages = ['jsm', 'lxml'],
+     )
